e2828a563e39caae0e7c14434c71c8cd5dbb9027
AidPaike <f335125303@163.com>
2023-03-01 19:59:22 +0800

    mysql conf

---
+++ utils/config.py
@@ -0,0 +1,8 @@
+# sql_config
+DATABASE_ADDRESS = 'comfuzz_mysql'
+DATABASE_PORT = 3306
+DATABASE_NAME = 'JSFuzzing'
+DATABASE_USER = 'root'
+DATABASE_PASSWORD = 'mysql123'
+# gpt_config
+MODEL_PATH = "/root/COMFUZZ/COMFUZZ_js/data/model/distilgpt2/checkpoint-640000"
