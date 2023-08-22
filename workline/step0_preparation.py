7bdd39c38bb028e17c21f4bed1b668825b15301b
aidpaike <f335125303@163.com>
2023-08-22 18:17:12 +0800

    v2.1 complate

--- workline/step0_preparation.py
+++ workline/step0_preparation.py
@@ -3,27 +3,27 @@ import shutil
 
 def extract_file(filename):
     try:
-        # 判断是否为压缩包文件
+        # Determine if the file is a zip file
         if filename.endswith('.tar'):
             process = subprocess.Popen(['tar', 'xvf', filename], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
             output, error = process.communicate()
         else:
             shutil.unpack_archive(filename, extract_dir='.', format=None)
 
-        # 输出解压状态
+        # Output decompression state
         if process.returncode == 0:
-            print('解压成功')
+            print('Unzip successfully')
         else:
-            print('解压失败')
+            print('Unzip failed')
 
-        # 输出解压过程中的信息或错误
+        # Output messages or errors during decompression
         if output:
             print(output.decode('utf-8'))
         if error:
             print(error.decode('utf-8'))
     except OSError as e:
-        print(f'解压错误: {e}')
+        print(f'decompression error: {e}')
 
-# 使用示例
+# usage example
 filename = '/root/jsvu.tar'
 extract_file(filename)
